3b12ee34969bd90219ac41a3d83000bf4dd6cd34
Alysson Lopes <alyssonlopes@Alyssons-MacBook-Pro.local>
2019-05-26 13:15:06 -0300

    Adjusting the project structure

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 5000
+
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/test', function (req, res) {
-    return res.send('Hello, Welcome to Upcoming Movies Web App!');
+app.get('/api/hello', function (req, res) {
+    return res.send({ hello: 'Hello, Welcome to Upcoming Movies Web App!'});
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.post('/api/test', (req, res) => {
+    console.log(req.body);
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.post}`
+    );
 });
 
-app.listen(process.env.PORT || 8080);
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
