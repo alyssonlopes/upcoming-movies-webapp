8b0f5ea8f4060df9a843178752ed4af926c64c3e
alysson.sousa <alysson.sousa@EDB-0003>
2019-05-29 16:36:52 -0300

    adjustments publish

--- client/src/services/movie.service.js
+++ client/src/services/movie.service.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL || ''
 
 export const getUpcomingMovies = async ({ page }) => {
     return await fetch(`${API_URL}/api/movies/${page}`);
